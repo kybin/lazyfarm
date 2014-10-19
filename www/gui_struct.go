20b5dbebd721dc8b4def98000a8622e641f21728
kim yongbin <kybinz@gmail.com>
2014-10-19 15:36:31 +0900

    first implement of gui_struct.go

--- www/gui_struct.go
+++ www/gui_struct.go
@@ -1,3 +1,22 @@
-//gui struct
+package www
+
+import (
+	"time"
+)
+
+type WWWJobInfo struct {
+	priority int // bigger number gets higher priority
+	name string
+	id string
+	user string
+}
+
+type WWWJobTime struct {
+	submit, start, end time.Time
+}
+
+type WWWJobStatus struct {
+	waiting, active, done, err string
+}
 
 
