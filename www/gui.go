b9c4ec03283f1592e53740917ef04fac1a81bfc5
kim han woong <khw7096@gmail.com>
2014-10-19 15:32:35 +0900

    chage text

--- www/gui.go
+++ www/gui.go
@@ -1,11 +1,9 @@
-//hi
-
 
 //farm status
-//ip, hostname, cpu, ram, currnt job : titlename, number
+//ip, hostname, cpu, ram, currnt job : name, current frame
 
 
 //job status
-//1st priority, name, %(gui에서 구현), status
+//1st priority, name, %(gui에서 구현), status(gui 구현), id, user
 //2nd submit time, start time, end time
-//3th id, user, waiting, active, error, done
+//3th errorNum, activeNum, waitingNum, doneNum
