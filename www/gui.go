0c4872e454aae1e235eff938bbb05cbd8ee915a8
kim han woong <khw7096@gmail.com>
2014-10-19 15:01:30 +0900

    add basic struct

--- www/gui.go
+++ www/gui.go
@@ -1,3 +1,11 @@
 //hi
 
 
+//farm status
+//ip, hostname, cpu, ram, currnt job : titlename, number
+
+
+//job status
+//1st priority, title, %, status
+//2nd submit time, start time, done time
+//3th id, user, waiting, active, error, done
